3834a8b370a3b8dc269dbc1dd02c6fc2a0a36dde
MichaelHabermas <michaelghabermas@gmail.com>
2021-06-08 18:48:04 -0400

    wip

--- api/cars/cars-middleware.js
+++ api/cars/cars-middleware.js
@@ -1,15 +1,22 @@
+const Cars = require('./cars-model');
+
 const checkCarId = (req, res, next) => {
-  // DO YOUR MAGIC
-}
+	Cars.getById(req.params.id)
+		.then(car => {
+			if (!car) {
+				next({
+					status: 404,
+					message: 'car with id <car id> is not found'
+				});
+			} else {
+				next();
+			}
+		})
+		.catch(next);
+};
 
-const checkCarPayload = (req, res, next) => {
-  // DO YOUR MAGIC
-}
+const checkCarPayload = (req, res, next) => {};
 
-const checkVinNumberValid = (req, res, next) => {
-  // DO YOUR MAGIC
-}
+const checkVinNumberValid = (req, res, next) => {};
 
-const checkVinNumberUnique = (req, res, next) => {
-  // DO YOUR MAGIC
-}
+const checkVinNumberUnique = (req, res, next) => {};
--- api/cars/cars-router.js
+++ api/cars/cars-router.js
@@ -1,1 +1,21 @@
-// DO YOUR MAGIC
+const router = require('express').Router();
+const Cars = require('./cars-model');
+const { getAll, getById, create } = require('./cars-middleware');
+
+router.get('/', (req, res, next) => {
+	Cars.getAll()
+		.then(cars => {
+			res.json(cars);
+		})
+		.catch(next);
+});
+
+router.get('/:id', (req, res, next) => {
+	Cars.getById().then().catch(next);
+});
+
+router.post('/', (req, res, next) => {
+	Cars.create(req.body)
+		.then(newCar => {})
+		.catch(next);
+});
